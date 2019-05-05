39fc310df7c33713360201406fffa0d1b24ce121
my pc <snehalbhongade7@gmail.com>
2019-05-06 00:25:00 +0530

    addition code added

--- ABC/src/A.java
+++ ABC/src/A.java
@@ -1,8 +1,22 @@
 
 public class A {
+	
+	public void m1()
+	{
+		System.out.println("I am in non static method");
+		int c=0;
+		int a=10;
+		int b=30;
+		c=a+b;
+		System.out.println(c);
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Github");
+		
+		A a = new A();
+		a.m1();
+		
 	}
 }
